8df5ae076cfd33d6b359c2572fcd5c4391c24d4f
Josemar amilton silva de almeida <amilton_cg12@hotmail.com>
2024-10-24 18:19:28 -0300

    corrigido bug se quantidade maior que os numeros

--- app.js
+++ app.js
@@ -5,20 +5,26 @@ function sortear() {
     let ate = parseInt(document.getElementById("ate").value);
 
     // array para armazena numeros aleatorios
-    let sorteados = []; 
+    let sorteados = [];
     let numero;
 
-    // loop para gerar numeros aleatorios
-    for (let i = 0; i < quantidade; i++) {
-        
-        numero = numeroAleatorio(de, ate);
-        
-        // loop para não escolher numeros repetidos
-        while (sorteados.includes(numero)) {
-            numero = numeroAleatorio(de, ate);
-        }
+    if (quantidade > de && quantidade > ate) {
+        alert("Campo Do número deve ser inferior ao campo Até o número. Verifique!");
+    } else {
 
-        sorteados.push(numero);
+        // loop para gerar numeros aleatorios
+        for (let i = 0; i < quantidade; i++) {
+
+            numero = numeroAleatorio(de, ate);
+
+            // loop para não escolher numeros repetidos
+            while (sorteados.includes(numero)) {
+                numero = numeroAleatorio(de, ate);
+            }
+
+            sorteados.push(numero);
+
+        }
 
     }
 
@@ -34,25 +40,25 @@ function numeroAleatorio(min, max) {
 }
 
 // função para alterar o estatus do botão reiniciar 
-function alterarBotao(){
-    
+function alterarBotao() {
+
     let reiniciar = document.getElementById("btn-reiniciar");
-    
-    if(reiniciar.classList.contains("container__botao-desabilitado")) {
+
+    if (reiniciar.classList.contains("container__botao-desabilitado")) {
         // removendo classe de tag
         reiniciar.classList.remove("container__botao-desabilitado");
         //adicionando class em tag
         reiniciar.classList.add("container__botao");
 
     } else {
-        
+
         reiniciar.classList.add("container__botao-desabilitado");
         reiniciar.classList.remove("container__botao");
     }
 }
 
 
-function reiniciar(){
+function reiniciar() {
     document.getElementById("quantidade").value = '';
     document.getElementById("de").value = '';
     document.getElementById("ate").value = '';
